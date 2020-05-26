f047e69214a3efb953ccbacbb21c2d310f0fd6d7
thomas <iam@tangkunyin.com>
2020-05-26 20:48:06 +0800

    add icestark base app
    Signed-off-by: thomas <iam@tangkunyin.com>

---
+++ icestark-demo/icestark-main/src/layouts/UserLayout/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function UserLayout({
+  children,
+}: {
+  children: React.ReactNode;
+  pathname?: string;
+}) {
+  return <div className="user-layout">{children}</div>;
+}
