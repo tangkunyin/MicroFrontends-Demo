13a0b9e59563c58b46e840e1dab00cfb72c71da9
thomas <iam@tangkunyin.com>
2020-06-02 16:36:14 +0800

    add annotation to reRouteUrl
    Signed-off-by: thomas <iam@tangkunyin.com>

--- single-spa-demo/src/thomas-root-config.ts
+++ single-spa-demo/src/thomas-root-config.ts
@@ -19,6 +19,13 @@ registerApplication({
   activeWhen: isActive.svelte,
 });
 
-start({
-  urlRerouteOnly: true,
-});
+start();
+
+/**
+ * 默认为false。如果设置为true，对history.pushState()和history.replaceState()的调用将不会触发single-spa路由重设。
+ * 除非客户端路由被更改，在某些情况下，将此设置为true可以提高性能
+ * see: https://github.com/single-spa/single-spa/issues/484
+ */
+// start({
+//   urlRerouteOnly: true,
+// });
